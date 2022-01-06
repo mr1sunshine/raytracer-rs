68bcc28e6e7c8aa6c2a2db8c70970e83bb5cbdd8
Aleksandr Ovchinnikov <mail@mr1sunshine.com>
2022-01-06 02:09:58 +0200

    Add Camera type and start generating pixels using multiple samples

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,6 +1,7 @@
+use rand::Rng;
 use raytracer::{
-    hittable::Hittable, hittable_list::HittableList, ray::Ray, sphere::Sphere, write_color, Color,
-    Point3, Vec3,
+    camera::Camera, color::write_color, hittable::Hittable, hittable_list::HittableList, ray::Ray,
+    sphere::Sphere, Color, Point3, Vec3,
 };
 use std::{env, fs::File, io::Write, process::exit};
 
@@ -18,6 +19,8 @@ fn ray_color(r: &Ray, world: &dyn Hittable) -> Color {
 }
 
 fn main() -> std::io::Result<()> {
+    let mut rng = rand::thread_rng();
+
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
         println!("Please specify output file as command line argument");
@@ -30,6 +33,7 @@ fn main() -> std::io::Result<()> {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+    const SAMPLES_PER_PIXEL: u32 = 100;
 
     // World
     let mut world = HittableList::default();
@@ -37,15 +41,7 @@ fn main() -> std::io::Result<()> {
     world.add(Box::new(Sphere::new(Point3::new(0.0, -100.5, -1.0), 100.0)));
 
     // Camera
-    const VIEWPORT_HEIGHT: f64 = 2.0;
-    const VIEWPORT_WIDTH: f64 = ASPECT_RATIO * VIEWPORT_HEIGHT;
-    const FOCAL_LENGTH: f64 = 1.0;
-
-    let origin = Point3::new(0.0, 0.0, 0.0);
-    let horizontal = Vec3::new(VIEWPORT_WIDTH, 0.0, 0.0);
-    let vertical = Vec3::new(0.0, VIEWPORT_HEIGHT, 0.0);
-    let lower_left_corner =
-        origin - horizontal / 2.0 - vertical / 2.0 - Vec3::new(0.0, 0.0, FOCAL_LENGTH);
+    let cam = Camera::new();
 
     // Render
 
@@ -57,12 +53,14 @@ fn main() -> std::io::Result<()> {
     // Image data
     for j in (0..=(IMAGE_HEIGHT - 1)).rev() {
         for i in 0..IMAGE_WIDTH {
-            let u = i as f64 / (IMAGE_WIDTH - 1) as f64;
-            let v = j as f64 / (IMAGE_HEIGHT - 1) as f64;
-            let dir = lower_left_corner + u * horizontal + v * vertical - origin;
-            let r = Ray::new(origin, dir);
-            let color = ray_color(&r, &world);
-            write_color(&mut file, &color)?;
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let u = (i as f64 + rng.gen_range(0.0..1.0)) / (IMAGE_WIDTH - 1) as f64;
+                let v = (j as f64 + rng.gen_range(0.0..1.0)) / (IMAGE_HEIGHT - 1) as f64;
+                let r = cam.get_ray(u, v);
+                pixel_color += ray_color(&r, &world);
+            }
+            write_color(&mut file, &pixel_color, SAMPLES_PER_PIXEL)?;
         }
     }
 
--- src/raytracer/lib.rs
+++ src/raytracer/lib.rs
@@ -1,20 +1,13 @@
-use std::fs::File;
-use std::io::Write;
-
 pub mod hittable;
 pub mod hittable_list;
 pub mod ray;
 pub mod sphere;
-mod vec3;
+pub mod camera;
+pub mod color;
+pub mod utility;
 
+mod vec3;
 pub type Vec3 = vec3::Vec3;
 pub type Point3 = vec3::Vec3;
 pub type Color = vec3::Vec3;
 
-pub fn write_color(file: &mut File, color: &Color) -> std::io::Result<()> {
-    let ir = (255.999 * color.x()) as i32;
-    let ig = (255.999 * color.y()) as i32;
-    let ib = (255.999 * color.z()) as i32;
-    writeln!(file, "{} {} {}", ir, ig, ib)?;
-    Ok(())
-}
