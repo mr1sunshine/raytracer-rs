4f9d27dffa5cff5ee372cd9a5e7b762201c2c024
Aleksandr Ovchinnikov <mail@mr1sunshine.com>
2022-01-08 00:55:51 +0200

    Add fuzed metal

--- src/raytracer/metal.rs
+++ src/raytracer/metal.rs
@@ -1,19 +1,30 @@
-use std::ptr::NonNull;
-
-use crate::{Color, material::Material, Vec3, ray::Ray};
+use crate::{material::Material, ray::Ray, Color, Vec3};
 
 pub struct Metal {
-    albedo: Color
+    albedo: Color,
+    fuzz: f64,
 }
 
 impl Metal {
-    pub fn new(albedo: &Color) -> Self { Self { albedo: *albedo } }
+    pub fn new(albedo: &Color, fuzz: f64) -> Self {
+        Self {
+            albedo: *albedo,
+            fuzz,
+        }
+    }
 }
 
 impl Material for Metal {
-    fn scatter(&self, r: &crate::ray::Ray, rec: &crate::hittable::HitRecord) -> Option<(crate::ray::Ray, Color)> {
+    fn scatter(
+        &self,
+        r: &crate::ray::Ray,
+        rec: &crate::hittable::HitRecord,
+    ) -> Option<(crate::ray::Ray, Color)> {
         let reflected = Vec3::reflect(&Vec3::unit_vector(&r.dir()), &rec.normal());
-        let scattered = Ray::new(rec.p(), reflected);
+        let scattered = Ray::new(
+            rec.p(),
+            reflected + self.fuzz * Vec3::random_in_unit_sphere(),
+        );
         if Vec3::dot(&scattered.dir(), &rec.normal()) > 0.0 {
             Some((scattered, self.albedo))
         } else {
